b4064f32fa88aa74f1856657e6f622f89602d962
Chris Lydon <chris.lydon@gmail.com>
2025-03-17 21:20:38 -0400

    signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <app-header />
+    <main>
+      <app-home />
+    </main>
     <router-outlet />
   `,
-  styles: [],
+  styles: [
+    `
+    main {
+      padding: 16px;
+
+    }
+    `
+  ],
 })
 export class AppComponent {
-  title = 'angular-todo';
 }
