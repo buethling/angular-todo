feb8b6e8cef1015b89d83b12faa1e06908460312
Chris Lydon <chris.lydon@gmail.com>
2025-03-24 17:33:13 -0400

    wrapping up todos by adding filtering

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,13 +1,19 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { TodoItem } from '../../model/todo.type';
+import { HighlightCompletedTodoDirective } from '../../directives/highlight-completed-todo.directive';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [HighlightCompletedTodoDirective],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss'
 })
 export class TodoItemComponent {
   todo = input.required<TodoItem>();
+  todoToggled = output<TodoItem>();
+
+  todoClicked() {
+    this.todoToggled.emit(this.todo());
+  }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,17 +2,21 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { TodoItem } from '../model/todo.type';
 import { catchError } from 'rxjs';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
   todoItems = signal<Array<TodoItem>>([]);
+  searchTerm = signal('');
 
   ngOnInit(): void {
     this.todoService.getTodosFromApi().pipe(
@@ -22,6 +26,20 @@ export class TodosComponent implements OnInit {
       })
     ).subscribe((todos) => {
       this.todoItems.set(todos);
+    });
+  }
+
+  updateTodoItem(todoItem: TodoItem) {
+    this.todoItems.update((todos) => {
+      return todos.map(todo => {
+        if (todo.id === todoItem.id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo;
+      });
     });
   }
 }
